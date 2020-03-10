282c5851d6466d283ddd7fc7f09cb1193751583f
Sinawat Jamsai <mr.icyland@gmail.com>
2020-03-10 12:06:55 +0700

    add encyption tool

---
+++ encryption/encryption.js
@@ -0,0 +1,6 @@
+import crypto from 'crypto';
+import { command } from 'yargs';
+
+const argv = command('keygen', 'Genterate encryption key for server and cloudflare api key encrypted while send request to server', {
+          
+}
